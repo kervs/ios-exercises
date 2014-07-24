425f526c6ba0a583ca3172cff173a46431093058
Kervins Valcourt <kervins.valcourt@gmail.com>
2014-07-24 11:40:01 -0400

    Completed strings

--- BlocExercises/BlocExercises/StringCheese.m
+++ BlocExercises/BlocExercises/StringCheese.m
@@ -12,17 +12,38 @@
 
 - (NSString *) favoriteCheeseStringWithCheese:(NSString *)cheeseName {
     /* WORK HERE */
-    return nil;
+    NSString *cheeseString = [NSString stringWithFormat: @"My favorite cheese is %@.", cheeseName];
+    
+    return cheeseString;
 }
 
 - (NSString *) cheeseNameWithoutCheeseSuffix:(NSString *)cheeseName {
     /* WORK HERE */
-    return nil;
+    
+    NSRange cheeseWord = [cheeseName rangeOfString:@" cheese" options:NSCaseInsensitiveSearch];
+    if (cheeseWord.location == NSNotFound) {
+        
+        return cheeseName;
+        
+    }
+    else{
+        NSString *justCheeseName = [cheeseName stringByReplacingCharactersInRange:cheeseWord withString:@""];
+        NSLog(@"%@",justCheeseName);
+        return justCheeseName;
+    }
+    
 }
 
 - (NSString *) numberOfCheesesStringWithCheeseCount:(NSUInteger)cheeseCount {
     /* WORK HERE */
-    return nil;
+    if (cheeseCount <= 1) {
+        NSString *cheeseNumberString = [NSString stringWithFormat:@"%i cheese",cheeseCount];
+        return cheeseNumberString;
+    }
+    else{
+    NSString *cheeseNumberString = [NSString stringWithFormat:@"%i cheeses",cheeseCount];
+    return cheeseNumberString;
+    }
 }
 
 @end
