289c66ddfeee81c6ca0a7247d8bfd52dc69def3e
Kervins Valcourt <kervins.valcourt@gmail.com>
2014-07-24 12:33:37 -0400

    Completed equality

--- BlocExercises/BlocExercises/EqualityDeterminer.m
+++ BlocExercises/BlocExercises/EqualityDeterminer.m
@@ -12,17 +12,20 @@
 
 - (BOOL) string:(NSString *)string1 isTheSameAsString:(NSString *)string2 {
     /* WORK HERE */
-    return (string1 == string2);
+    BOOL areStringEqual = [string1 isEqualToString:string2];
+    return areStringEqual;
 }
 
 - (BOOL) number:(NSNumber *)number1 isTheSameAsNumber:(NSNumber *)number2 {
     /* WORK HERE */
-    return (number1 == number2);
+    BOOL areNumbersEqual = [number1 isEqualToNumber:number2];
+    return areNumbersEqual;
 }
 
 - (BOOL) integer:(NSInteger)integer1 isGreaterThan:(NSInteger)integer2 {
     /* WORK HERE */
-    return NO;
+    BOOL greaterThan = integer1 > integer2;
+    return greaterThan;
 }
 
 @end
