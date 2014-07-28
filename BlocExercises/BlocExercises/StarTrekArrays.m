cd872a1c18fb25ea07721b8cc574118c776b0684
Kervins Valcourt <kervins.valcourt@gmail.com>
2014-07-28 17:28:05 -0400

    Completed arrays

--- BlocExercises/BlocExercises/StarTrekArrays.m
+++ BlocExercises/BlocExercises/StarTrekArrays.m
@@ -12,22 +12,31 @@
 
 - (NSArray *) arrayOfStarTrekCharactersFromString:(NSString *)characterString {
     /* WORK HERE */
-    return @[];
+   NSArray *starTrekArray = [characterString componentsSeparatedByString:@";"];
+    return starTrekArray;
 }
 
 - (NSString *) stringOfStarTrekCharactersFromArray:(NSArray *)characterArray {
     /* WORK HERE */
-    return @"";
+    NSString *starTrekString = [NSString stringWithFormat:@"%@;%@;%@",characterArray[0],characterArray[1],characterArray[2]];
+    return starTrekString;
 }
 
 - (NSArray *) alphabeticallySortedStarTrekCharactersFromArray:(NSArray *)characterArray {
     /* WORK HERE */
-    return @[];
+    NSSortDescriptor *starTrekSort = [[NSSortDescriptor alloc]initWithKey:nil ascending:YES selector:@selector(localizedCaseInsensitiveCompare:)];
+    return [characterArray sortedArrayUsingDescriptors:@[starTrekSort]];
 }
 
 - (BOOL) characterArrayContainsWorf:(NSArray *)characterArray {
     /* WORK HERE */
-    return NO;
-}
+    
+    NSPredicate *containWorf = [NSPredicate predicateWithFormat:@"SELF CONTAINS[c] 'worf'"];
+    if ([characterArray filteredArrayUsingPredicate:containWorf].count == 0 ) {
+        return NO;
+    } else
+        return YES;}
+
+
 
 @end
