3f1e100f1f89445044be64005510267b6bac5ea4
Kervins Valcourt <kervins.valcourt@gmail.com>
2014-07-29 00:45:47 -0400

    Completed simple data types and objects

--- BlocExercises/BlocExercises/NumberHandler.m
+++ BlocExercises/BlocExercises/NumberHandler.m
@@ -12,17 +12,29 @@
 
 - (NSNumber *) numberThatIsTwiceAsBigAsNumber:(NSNumber *)number {
     /* WORK HERE */
-    return @0;
+    NSNumber *newNumber = [NSNumber numberWithInt:([number integerValue] *2)];
+    return newNumber;
 }
 
 - (NSArray *) arrayOfNumbersBetweenNumber:(NSInteger)number andOtherNumber: (NSInteger)otherNumber {
     /* WORK HERE */
-    return @[];
+    int secoundNumber = otherNumber;
+    NSMutableArray *listOfNumbers = [[NSMutableArray alloc]init];
+    for ( int i = number; i <= secoundNumber; i++) {
+        [listOfNumbers addObject:[NSNumber numberWithInt:i]];
+        
+    }
+    
+    return listOfNumbers;
 }
 
 - (NSInteger) lowestNumberInArray:(NSArray *)arrayOfNumbers {
     /* WORK HERE */
-    return 0;
+    NSMutableArray *orderNumberArray = [[NSMutableArray alloc]initWithArray:arrayOfNumbers];
+    NSSortDescriptor *sortDescriptor = [[NSSortDescriptor alloc]initWithKey:nil ascending:YES];
+    [orderNumberArray sortUsingDescriptors:@[sortDescriptor]];
+
+    return [orderNumberArray[0] integerValue];
 }
 
 @end
